abd7b6ffcfdb0aa8a6147dd00c4a432dfe76adfe
Guilherme Fernandes <gfernandes@gazeus.com>
2020-05-09 20:46:32 -0300

    Fixing need for header.cs

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
 ﻿const gulp = require('gulp');
 const concat = require('gulp-concat');
+const header = require('gulp-header');
+const data = require('gulp-data');
 const modify = require('gulp-modify-file');
-const path = require('path');
 
-gulp.task('default', function () {
+const headers = [];
+
+function unique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+gulp.task('compile', function () {
     return gulp.src([
-        './src/header.cs',
-        './src/Vector2i.cs',
-        './src/PriorityQueue.cs',
-        './src/IPathfinder.cs',
-        './src/AStarPathfinder.cs',
-        './src/Model.cs',
-        './src/Core.cs',
-        './src/Player.cs'
+        './src/common/**/*.cs',
+        './src/bot/**/*.cs'
     ])
         .pipe(modify((content, filePath, file) => {
 
-            if (path.basename(filePath) !== 'header.cs')
-                return content.replace(/using.*/g, "");
-            else
-                return content;
+            const regex = /using.*\n/g;
+
+            const imports = content.match(regex);
+
+            if(imports)
+                headers.push(...imports);
+            
+            return content.replace(regex, "");
         }))
-        .pipe(concat('Build.cs', { newLine: '\n\n' }))
+        .pipe(concat('Build.cs'))
         .pipe(gulp.dest('./build/'));
 });
+
+gulp.task('header', function () {
+    return gulp.src(['./build/Build.cs'])
+        .pipe(header(headers.filter(unique).join('')))
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('default', gulp.series('compile', 'header'));
